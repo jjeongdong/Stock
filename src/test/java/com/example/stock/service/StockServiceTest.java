5836850c8760e2175eaefbe1568a4518c8943fe8
jjeongdong <donghun3296@naver.com>
2024-06-27 23:28:10 +0900

    낙관적 락 & 비관적 락 적용

---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,72 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StockServiceTest {
+
+    @Autowired
+    private PessimisticLockStockService pessimisticLockStockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        stockRepository.save(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 재고감소() {
+        pessimisticLockStockService.decrease(1L, 1L);
+
+        Stock stock = stockRepository.findById(1L)
+                .orElseThrow(EntityNotFoundException::new);
+
+        assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    @DisplayName("동시에 100개의 재고 감소 요청")
+    void decrease_concurrently()  throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    pessimisticLockStockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L)
+                .orElseThrow(EntityNotFoundException::new);
+        assertEquals(0, stock.getQuantity());
+    }
+
+}
